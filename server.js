3dbab7dc3f358c3654f6e0255ab42177e511cef3
jtsuero <9836321+jtsuero@users.noreply.github.com>
2018-10-02 21:55:13 -0700

    Update server.js

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var server = http.createServer();
 server.on("request", function (req, res) {
   //console.log("we are hitting this");
   res.write("hello world");
+  res.write("guess who's back");
   res.end();
 });
 
